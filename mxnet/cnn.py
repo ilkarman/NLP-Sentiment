2c9571cfa8006421617a9961d17d820ab04ed575
Ilia Karmanov <ilkarman@microsoft.com>
2016-08-21 01:40:50 +0100

    Updated Crepe Model
    Still not working - takes way too much memory but setup looks ok

--- mxnet/cnn.py
+++ mxnet/cnn.py
@@ -1,34 +1,68 @@
+"""
+Attempt to replicate torch model using MXNET:
+https://github.com/zhangxiangxiao/Crepe
+
+"To run this example succesfully you will also need a NVidia GPU with at
+least 3GB of memory.
+Otherwise, you can configure the model in train/config.lua for less parameters."
+
+ISSUE: mxnet requires way more than 3GB memory for the model ...
+
+ERROR:
+[01:38:23] D:\chhong\mxnet\dmlc-core\include\dmlc/logging.h:235: [01:38:23] d:\chhong\mxnet\src\storage\
+./gpu_device_storage.h:39: Check failed: e == cudaSuccess || e == cudaErrorCudartUnloading CUDA: out of memory
+Traceback (most recent call last):
+  File "C:/Users/superuser/PycharmProjects/cnn_crepe/cnn.py", line 190, in <module>
+    label_shapes=[('softmax_label', (BATCH_SIZE,))])
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\module\module.py", line 256, in bind
+    shared_group, logger=self.logger)
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\module\executor_group.py", line 95, in __init__
+    self.bind_exec(data_shapes, label_shapes, shared_group)
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\module\executor_group.py", line 123, in bind_exec
+    self.execs.append(self._bind_ith_exec(i, data_shapes, label_shapes, shared_group))
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\module\executor_group.py", line 379, in _bind_ith_exec
+    arg_arr = nd.zeros(arg_shapes[j], context, dtype=arg_types[j])
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\ndarray.py", line 688, in zeros
+    arr = empty(shape, ctx, dtype)
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\ndarray.py", line 513, in empty
+    return NDArray(handle=_new_alloc_handle(shape, ctx, False, dtype))
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\ndarray.py", line 65, in _new_alloc_handle
+    ctypes.byref(hdl)))
+  File "C:\Anaconda2\lib\site-packages\mxnet-0.7.0-py2.7.egg\mxnet\base.py", line 77, in check_call
+    raise MXNetError(py_str(_LIB.MXGetLastError()))
+mxnet.base.MXNetError: [01:38:23] d:\chhong\mxnet\src\storage\./gpu_device_storage.h:39: Check failed:
+e == cudaSuccess || e == cudaErrorCudartUnloading CUDA: out of memory
+
+"""
+
 import numpy as np
 import pandas as pd
 import mxnet as mx
 import wget
 import time
 import os.path
+from mxnet.io import DataBatch
 
+ctx = mx.gpu(0)  # Run on one GPU
 
 AZ_ACC = "amazonsentimenik"
 AZ_CONTAINER = "textclassificationdatasets"
 ALPHABET = list("abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+ =<>()[]{}")
-print("Alphabet %d characters: " % len(ALPHABET), ALPHABET)
-FEATURE_LEN = 1014
-######################################################
-# These have been scaled down due to computer limits
-BATCH_SIZE = 64  # 128 is too big!!
-NUM_FILTERS = 128  # 256 is too big!!!
-EPOCHS = 100  # config.main.epoches = 5000
-######################################################
-SD = 0.05  # stdev for gaussian distribution
+
+FEATURE_LEN = 1014  # First 1014 characters
+BATCH_SIZE = 8  # 128 is too big!!
+NUM_FILTERS = 16  # 256 is too big!!!
+EPOCHS = 1000  # config.main.epoches = 5000
+SD = 0.05  # std for gaussian distribution
 NOUTPUT = 2  # good or bad
 DATA_SHAPE = (BATCH_SIZE, 1, FEATURE_LEN, len(ALPHABET))
 
-# CPU or GPU
-ctx = mx.cpu()
+print("Alphabet %d characters: " % len(ALPHABET), ALPHABET)
 
 
 def download_file(url):
     # Create file-name
     local_filename = url.split('/')[-1]
-
     if os.path.isfile(local_filename):
         pass
         # print("The file %s already exist in the current directory\n" % local_filename)
@@ -40,35 +74,26 @@ def download_file(url):
 
 
 def load_data_frame(infile, batch_size=128, shuffle=True):
-
-    print("processing data frame")
-
+    print("processing data frame: %s" % infile)
     # Get data from windows blob
     download_file('https://%s.blob.core.windows.net/%s/%s' % (AZ_ACC, AZ_CONTAINER, infile))
-
     # load data into dataframe
     df = pd.read_csv(infile,
                      header=None,
                      names=['sentiment', 'summary', 'text'])
-
     # concat summary, review; trim to 1014 char; reverse; lower
     df['rev'] = df.apply(lambda x: "%s %s" % (x['summary'], x['text']), axis=1)
     df.rev = df.rev.str[:FEATURE_LEN].str[::-1].str.lower()
     # store class as nparray
     df.sentiment -= 1
     y_split = np.asarray(df.sentiment, dtype='int')
-    # print(Y_split[:30])
     # drop columns
     df.drop(['text', 'summary', 'sentiment'], axis=1, inplace=True)
-
     if shuffle:
         df = df.sample(frac=1).reset_index(drop=True)
-
     # Dictionary to create character vectors
     character_hash = pd.DataFrame(np.identity(len(ALPHABET)), columns=ALPHABET)
-
-    print("finished processing data frame")
-
+    print("finished processing data frame: %s" % infile)
     # Yield mini-batch amount of character vectors
     X_split = np.zeros([batch_size, 1, FEATURE_LEN, len(ALPHABET)], dtype='int')
     for ti, tx in enumerate(df.rev):
@@ -85,16 +110,16 @@ def load_data_frame(infile, batch_size=128, shuffle=True):
 
 def example(infile='amazon_review_polarity_test.csv'):
     mbatch = 3
-    df = pd.read_csv(infile, header=None)
-    train_y = df[[0]].values.ravel() - 1
-    print("actual: ", train_y[:mbatch * 4])
-
     counter = 0
     for batchX, batchY in load_data_frame(infile, batch_size=mbatch, shuffle=False):
         print("batch: ", batchY.asnumpy().astype('int32'))
         counter += 1
         if counter == 4:
             break
+
+    df = pd.read_csv(infile, header=None)
+    train_y = df[[0]].values.ravel() - 1
+    print("actual: ", train_y[:mbatch * 4])
 
 
 def create_crepe():
@@ -183,84 +208,67 @@ def create_crepe():
     return crepe
 
 
-# Get symbols and devices
-
+# Create mx.mod.Module()
 cnn = create_crepe()
+mod = mx.mod.Module(cnn, context=ctx)
 
-# Set-up model
+mod.bind(data_shapes=[('data', DATA_SHAPE)],
+         label_shapes=[('softmax_label', (BATCH_SIZE,))])
 
-# Manually set-up the training process
-input_shapes = {'softmax_label': (BATCH_SIZE,),
-                'data': DATA_SHAPE}
+mod.init_params(mx.init.Normal(sigma=SD))
+mod.init_optimizer(optimizer='sgd',
+                   optimizer_params={
+                       "learning_rate": 0.01,
+                       "momentum": 0.9,
+                       "wd": 0.00001,
+                       "rescale_grad": 1.0/BATCH_SIZE
+                   })
 
-# Attach symbol to executor
-exe = cnn.simple_bind(ctx=ctx, **input_shapes)
 
-# Create link to data and label (to symbol)
-arg_arrays = dict(zip(cnn.list_arguments(), exe.arg_arrays))
-data = exe.arg_dict['data']
-label = exe.arg_dict['softmax_label']
-
-# init weights
-init = mx.init.Normal(sigma=SD)
-for name, arr in arg_arrays.items():
-    if name not in input_shapes:
-        init(name, arr)
-
-# Stochastic gradient descent
-opt = mx.optimizer.SGD(
-    learning_rate=0.01,
-    momentum=0.9,
-    wd=0.00001,
-    rescale_grad=1.0 / BATCH_SIZE)
-
-updater = mx.optimizer.get_updater(opt)
+def test_net(epoch):
+    """" Assess perf on test every epoch ... should really have validatoin data """
+    metric = mx.metric.Accuracy()
+    print("started testing - epoch %d" % epoch)
+    for batchX, batchY in load_data_frame('amazon_review_polarity_test.csv', batch_size=BATCH_SIZE):
+        batch = DataBatch(data=[batchX], label=[batchY])
+        mod.forward(batch)
+        mod.update_metric(metric, batch.label)
+    metric_m, metric_v = metric.get()
+    print("TEST(%s): %.4f" % (metric_m, metric_v))
 
 # Train
 print("started training")
 tic = time.time()
-
 # Evaluation metric:
 metric = mx.metric.Accuracy()
-
 # Train EPOCHS
 for epoch in range(EPOCHS):
     t = 0
-    for batchX, batchY in load_data_frame('amazon_review_polarity_train.csv', batch_size=BATCH_SIZE, shuffle=True):
-        # Copy data to executor input
-        data[:] = batchX
-        label[:] = batchY
-        # forward
-        exe.forward(is_train=True)
-        # backward
-        exe.backward()
-        # Update
-        for i, pair in enumerate(zip(exe.arg_arrays, exe.grad_arrays)):
-            weight, grad = pair
-            updater(i, grad, weight)
-        # get metric
-        metric.update([batchY], exe.outputs)
-        # log every 100 batches
+    metric.reset()
+    for batchX, batchY in load_data_frame('amazon_review_polarity_train.csv',
+                                          batch_size=BATCH_SIZE,
+                                          shuffle=True):
+        # Wrap X and y in DataBatch (need to be in a list)
+        batch = DataBatch(data=[batchX], label=[batchY])
+        # Push data forwards and update metric
+        # For training + testing
+        mod.forward(batch)
+        mod.update_metric(metric, batch.label)
+        # Get weights and update
+        # For training only
+        mod.backward()
+        mod.update()
+        # Log every 10 batches
         t += 1
-        if t % BATCH_SIZE == 0:
+        if t % 1 == 0:
             toc = time.time()
             train_t = toc - tic
             metric_m, metric_v = metric.get()
             print("epoch: %d iter: %d metric(%s): %.4f dur: %.0f" % (epoch, t, metric_m, metric_v, train_t))
 
-print("Finished training in %.0f seconds" % (time.time() - tic))
+    print("Finished epoch, testing ...")
+    test_net(epoch)
 
-# Test
-metric = mx.metric.Accuracy()
+print("Finished in %.0f seconds" % (time.time() - tic))
 
-print("started testing")
-for batchX, batchY in load_data_frame('amazon_review_polarity_test.csv', batch_size=BATCH_SIZE):
-    data[:] = batchX
-    label[:] = batchY
-    # forward
-    exe.forward(is_train=False)
-    # metric for prediction
-    metric.update([batchY], exe.outputs)
 
-metric_m, metric_v = metric.get()
-print("metric(%s): %.4f" % (metric_m, metric_v))
